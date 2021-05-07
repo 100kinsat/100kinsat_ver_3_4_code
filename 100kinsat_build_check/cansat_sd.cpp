faea851bda36300c438527608267f02256521fb2
Yamato <yamamto117@gmail.com>
2021-05-07 10:34:20 +0900

    feat: SDカード用ライブラリを追加

---
+++ 100kinsat_build_check/cansat_sd.cpp
@@ -0,0 +1,118 @@
+// Reference:
+// https://github.com/espressif/arduino-esp32/tree/master/libraries/SD
+#include "cansat_sd.hpp"
+
+/**
+ * @brief SDカードの初期化処理
+ *
+ */
+CanSatSd::CanSatSd() {
+  Serial.begin(115200);
+  if (!SD.begin()) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if (cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if (cardType == CARD_MMC) {
+    Serial.println("MMC");
+  } else if (cardType == CARD_SD) {
+    Serial.println("SDSC");
+  } else if (cardType == CARD_SDHC) {
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+}
+
+/**
+ * @brief ファイルを読み込む
+ *
+ * @param fs
+ * @param path
+ */
+void CanSatSd::readFile(fs::FS &fs, const char *path) {
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while (file.available()) {
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+/**
+ * @brief ファイルに書き込む（上書き）
+ *
+ * @param fs
+ * @param path
+ * @param message
+ */
+void CanSatSd::writeFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+/**
+ * @brief ファイルに書き込む（追記）
+ *
+ * @param fs
+ * @param path
+ * @param message
+ */
+void CanSatSd::appendFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+/**
+ * @brief ファイルを削除する
+ *
+ * @param fs
+ * @param path
+ */
+void CanSatSd::deleteFile(fs::FS &fs, const char *path) {
+  Serial.printf("Deleting file: %s\n", path);
+  if (fs.remove(path)) {
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+}
